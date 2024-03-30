46be08d8a7228e5bcea26307f5100f324ba58169
segunojo1 <olusegunkd2017@gmail.com>
2024-03-30 23:47:46 +0100

    fix

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -17,11 +17,11 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0:
+        if (i % 3) == 0 and (i % 3) != 0:
             tmp_result.append("Fizz")
-        if (i % 3) == 0 and (i % 5) == 0:
+        elif (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
-        if (i % 5) == 0:
+        elif (i % 5) == 0 and (i % 3) != 0:
             tmp_result.append("Buzz")
         else:
             tmp_result.append(str(i))
@@ -37,4 +37,3 @@ if __name__ == '__main__':
 
     number = int(sys.argv[1])
     fizzbuzz(number)
-    
